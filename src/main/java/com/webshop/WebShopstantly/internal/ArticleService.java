0b9d77b39ad4b9309b782b4ec20c2cc872905cf4
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-17 02:05:44 +0100

    packing slip as text file output

--- src/main/java/com/webshop/WebShopstantly/internal/ArticleService.java
+++ src/main/java/com/webshop/WebShopstantly/internal/ArticleService.java
@@ -3,6 +3,7 @@ package com.webshop.WebShopstantly.internal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.PrintStream;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,12 +19,20 @@ public class ArticleService {
         Article articleToRecude = wantedArticle.orElseThrow(NullPointerException::new);
         articleToRecude.setStock(articleToRecude.getStock() - number);
         articleRepository.save(articleToRecude);
+        try {
+            PrintStream fileStream = new PrintStream(Math.random()+".txt");
+            System.setOut(fileStream);
+        }catch(Exception e){
+            System.out.println(e.toString());
+        }
         System.out.println(this.printPackingSlip(articleToRecude, number));
     }
 
     public String printPackingSlip(Article article, int stock){
        String place = article.getPlace();
        String packingSlip = "----------------------------------------------\n Packing Slip für Artikel:\t| "+article.getName()+"\n Anzahl:\t\t\t\t\t| "+stock+"\n Ort:\t\t\t\t\t\t| "+place+"\n----------------------------------------------";
+
+
        return packingSlip;
     }
 }
